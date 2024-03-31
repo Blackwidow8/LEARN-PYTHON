05a1812aebbb35c2a0848bc91d8226c36f24361e
Susan Wafula <ariellaroxxy@gmail.com>
2024-03-31 16:30:54 +0300

    dictionaries

---
+++ WEEK2/day2.py
@@ -0,0 +1,22 @@
+#Dictionaries
+
+#The dict() class
+my_values = dict()
+person={
+    "name":"Dan",
+    "school": "Zindua School",
+    "age": 16,
+    "hobbies":["Basketball", "Swimming", "Football"],
+    "isActive":True,
+    "isPresent":False,
+    "other_values":{
+        "personalities":None,
+    }
+    
+}
+
+person["name"]='Walobwa'
+person ["age"]+= 1
+
+
+print(person)
